9961e0ecd949eb6cb4c8734b2b3bbf4a77067cac
premanterminal <premanterminal@sffs.mshome.net>
2022-06-29 14:36:28 +0700

    first commit

---
+++ app/src/main/java/com/example/trial_nfc_android_java/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.trial_nfc_android_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
